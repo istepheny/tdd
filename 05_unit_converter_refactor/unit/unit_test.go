d69fd114c83123377b2482603bb353294c305672
stephen.yang <stephen.yang@ucloud.cn>
2022-10-21 23:35:46 +0800

    add Coverage

--- 05_unit_converter_refactor/unit/unit_test.go
+++ 05_unit_converter_refactor/unit/unit_test.go
@@ -1,7 +1,15 @@
 package unit
 
-import "testing"
+import (
+	"github.com/stretchr/testify/assert"
+	"testing"
+)
 
 func TestUnit(t *testing.T) {
-
+	assert.Equal(t, "Inch", Inch.Name())
+	assert.Equal(t, 1, Inch.TransferRateToInch())
+	assert.Equal(t, "Foot", Foot.Name())
+	assert.Equal(t, 12, Foot.TransferRateToInch())
+	assert.Equal(t, "Yard", Yard.Name())
+	assert.Equal(t, 36, Yard.TransferRateToInch())
 }
--- README.md
+++ README.md
@@ -1,5 +1,7 @@
 # [熊节的 TDD 练功房](https://mp.weixin.qq.com/mp/appmsgalbum?action=getalbum&__biz=MzU1MTkyNDU4MA==&scene=1&album_id=1539408163689594884)
 
+[![Coverage Status](https://coveralls.io/repos/github/istepheny/tdd/badge.svg)](https://coveralls.io/github/istepheny/tdd)
+
 1. [fizzbuzz](./01_fizzbuzz)
 2. [fizzbuzz 重构版](./02_fizzbuzz_refactor)
 3. [fizzbuzz 需求扩展版](./03_fizzbuzz_extend)
